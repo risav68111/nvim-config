f05e55c0cae3329aa1a5a51f769f22a623410d22
risav68111 <rishav.kumar.68111@gmail.com>
2025-03-08 01:48:53 +0530

    basic setup to work

---
+++ init.lua
@@ -0,0 +1,3 @@
+ require('config')
+-- require("kickstart")
+print("Nvim started...")
---
+++ lua/config/plugins/undo-tree.lua
@@ -0,0 +1,13 @@
+return {
+    "mbbill/undotree",
+    config = function()
+        -- Disable the diff command to prevent errors
+        vim.g.undotree_DiffAutoOpen = 0
+
+        -- Keybinding: Press <leader> + u to toggle Undotree
+        vim.keymap.set("n", "<leader><F5>", function()
+            vim.cmd("UndotreeToggle")
+        end, { noremap = true, silent = true })
+    end
+}
+
