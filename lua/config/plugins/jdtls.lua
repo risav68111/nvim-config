9837c6ac404a2b250eadedf5445387c240db1d90
risav <98260251+risav68111@users.noreply.github.com>
2025-03-23 20:12:39 +0530

    Update jdtls.lua
    shortcut for getter/setter, constructor and tostring function

--- lua/config/plugins/jdtls.lua
+++ lua/config/plugins/jdtls.lua
@@ -1,3 +1,8 @@
 return {
   { "mfussenegger/nvim-jdtls" },
+config = function() 
+    vim.keymap.set("n", "<leader>gs", "<Cmd>lua require('jdtls').code_action()<CR>", { desc = "Generate Getters/Setters" })
+    vim.keymap.set("n", "<leader>gc", "<Cmd>lua require('jdtls').code_action()<CR>", { desc = "Generate Constructor" })
+    vim.keymap.set("n", "<leader>gt", "<Cmd>lua require('jdtls').code_action()<CR>", { desc = "Generate toString()" })
+  end,
 }
