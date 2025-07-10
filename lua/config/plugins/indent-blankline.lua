4cd865d58efa0b0c555e0521d96d4aff65eafd16
msi.risav68111 <rishav.kumar.68111@gmail.com>
2025-07-10 22:38:50 +0530

    added plugin and indent change

---
+++ lua/config/plugins/indent-blankline.lua
@@ -0,0 +1,30 @@
+-- visual_scope.lua
+
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = { char = "│" },
+      scope = {
+        enabled = true,
+        show_start = true,
+        show_end = true,
+        highlight = { "Function", "Label" },
+      },
+    }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      enable = true,            -- Show function/class name at top
+      throttle = true,
+      max_lines = 3,            -- Max lines of context to show
+      multiline_threshold = 20,
+      trim_scope = 'outer',
+      mode = 'cursor',          -- Cursor-based context
+      separator = nil,
+    }
+  }
+}
+
