a87a0650454d4cebdb003cf305ef2b3c7b60f3c6
risav (_) arch <rishav.kumar.68111@gmail.com>
2026-06-02 21:16:45 +0530

    highlight vim conf update

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -6,12 +6,12 @@ require "config.shell"
 -- require 'config.health'
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
 })
 
 -- vim.opt.runtimepath:remove("/usr/lib/nvim")
-vim.treesitter.language.add("go")
+-- vim.treesitter.language.add("go")
