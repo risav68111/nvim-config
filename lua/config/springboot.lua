9994f8810746b877fc473b811500fad2ac0c18b6
risav (_) arch <rishav.kumar.68111@gmail.com>
2026-08-16 18:29:27 +0530

    build java fn, etc

--- lua/config/springboot.lua
+++ lua/config/springboot.lua
@@ -18,44 +18,43 @@ local function file_contains(path, pattern)
 end
 
 function M.run()
-
-  local has_pom        = file_exists("pom.xml")
+  local has_pom       = file_exists("pom.xml")
   local has_gradle    = file_exists("build.gradle") or file_exists("build.gradle.kts")
   local has_mvnw      = file_exists("mvnw")
   local has_gradlew   = file_exists("gradlew")
 
   local is_springboot =
-    file_contains("pom.xml", "spring%-boot")
-    or file_contains("build.gradle", "spring%-boot")
-    or file_contains("build.gradle.kts", "spring%-boot")
+      file_contains("pom.xml", "spring%-boot")
+      or file_contains("build.gradle", "spring%-boot")
+      or file_contains("build.gradle.kts", "spring%-boot")
 
   local cmd
 
   if is_springboot then
     if has_gradle then
       cmd = has_gradlew
-        and { "./gradlew", "bootRun" }
-        or { "gradle", "bootRun" }
+          and { "./gradlew", "bootRun" }
+          or { "gradle", "bootRun" }
     else
       if not has_pom then
         return
       end
       cmd = has_mvnw
-        and { "./mvnw", "spring-boot:run" }
-        or { "mvn", "spring-boot:run" }
+          and { "./mvnw", "spring-boot:run" }
+          or { "mvn", "spring-boot:run" }
     end
   else
     if has_gradle then
       cmd = has_gradlew
-        and { "./gradlew", "run" }
-        or { "gradle", "run" }
+          and { "./gradlew", "run" }
+          or { "gradle", "run" }
     else
       if not has_pom then
         return
       end
       cmd = has_mvnw
-        and { "./mvnw", "clean", "compile", "exec:java" }
-        or { "mvn", "clean", "compile", "exec:java" }
+          and { "./mvnw", "clean", "compile", "exec:java" }
+          or { "mvn", "clean", "compile", "exec:java" }
     end
   end
 
@@ -67,5 +66,33 @@ function M.run()
   vim.cmd("startinsert")
 end
 
+function M.build()
+  local has_pom     = file_exists("pom.xml")
+  local has_gradle  = file_exists("build.gradle") or file_exists("build.gradle.kts")
+  local has_mvnw    = file_exists("mvnw")
+  local has_gradlew = file_exists("gradlew")
+
+  local cmd
+
+  if has_gradle then
+    cmd = has_gradlew
+        and { "./gradlew", "clean", "build" }
+        or { "gradle", "clean", "build" }
+  else
+    if not has_pom then
+      return
+    end
+    cmd = has_mvnw
+        and { "./mvnw", "clean", "package" }
+        or { "mvn", "clean", "package" }
+  end
+
+  vim.cmd("wa")
+  local buf = vim.api.nvim_create_buf(true, false)
+  vim.api.nvim_win_set_buf(0, buf)
+
+  vim.fn.termopen(cmd)
+  vim.cmd("startinsert")
+end
 
 return M
